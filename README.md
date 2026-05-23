e885018b7c14b84a92bc094eb98c4004f1a684bb
hdarlim <henriquedarlim@gmail.com>
2026-05-23 18:16:39 -0300

    Refactor Cliente class to include id_cliente and senha attributes; update usage in main.py and README.md

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Uso rápido (exemplo):
 ```python
 from modelos_teste import Cliente, ItemPedido, Pedido, Produto
 
-novo_cliente = Cliente("João", "Rua dos Bobos, 0", "119928342", "email@example.com")
+novo_cliente = Cliente(1, "João", "Rua dos Bobos, 0", "119928342", "email@example.com", "senhacompletamentesegura")
 produto = Produto(456, "Iogurte natural", "Fixo", "Iogurte natural sem conservantes feito com muito amor!")
 item = ItemPedido(1, 20.0, 3)
 pedido = Pedido(123, "2026-05-21", "Pago", item.total_item())
--- modelos_teste.py
+++ modelos_teste.py
@@ -1,16 +1,18 @@
 class Cliente:
-    def __init__(self, nome: str, endereco: str, telefone: str, email: str) -> None:
+    def __init__(self, id_cliente: int, nome: str, endereco: str, telefone: str, email: str, senha: str) -> None:
+        self.id_cliente: int = id_cliente # chave primária
         self.nome: str = nome
         self.endereco: str = endereco
         self.telefone: str = telefone
         self.email: str = email
+        self.senha: str = senha
 
     def __str__(self) -> str:
-        return f"Nome: {self.nome}, Endereço: {self.endereco}, Telefone: {self.telefone}, Email: {self.email}"
+        return f"Id_cliente: {self.id_cliente}, Nome: {self.nome}, Endereço: {self.endereco}, Telefone: {self.telefone}, Email: {self.email}, Senha: {self.senha}"
     
 class Pedido:
     def __init__(self, codigo: int, data: str, status: str, total: float) -> None:
-        self.codigo: int = codigo
+        self.codigo: int = codigo # chave primária
         self.data: str = data
         self.status: str = status
         self.total: float = total
@@ -20,7 +22,7 @@ class Pedido:
 
 class ItemPedido:
     def __init__(self, id_item: int, preco: float, quantidade: int) -> None:
-        self.id_item: int = id_item
+        self.id_item: int = id_item # chave primária
         self.preco: float = preco
         self.quantidade: int = quantidade
 
@@ -32,7 +34,7 @@ class ItemPedido:
     
 class Produto:
     def __init__(self, id_produto: int, nome: str, tipo: str, descricao: str) -> None:
-        self.id_produto: int = id_produto
+        self.id_produto: int = id_produto # chave primária
         self.nome: str = nome
         self.tipo: str = tipo # esse atributo pode ter dois valores: fixo ou variável
         self.descricao: str = descricao
